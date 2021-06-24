adc52793828b0676e341477cd5f53455ef9419d6
unknown <locbgiang@gmail.com>
2021-06-23 22:39:49 -0700

    adding comments

--- Lesson_1/Contract.sol
+++ Lesson_1/Contract.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint zombieId, string name, uint dna);      // events are a way for contract to communicate that something happened on the blockchain to your app
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -13,17 +13,17 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna));
-        emit NewZombie(id, _name, _dna);
+        uint id = zombies.push(Zombie(_name, _dna))-1;          // getting the id the length of the array-1
+        emit NewZombie(id, _name, _dna);                        // firing the event
     }
 
     function _generateRandomDna(string memory _str) private view returns(uint){
-        uint rand = uint(keccak256(abi.encodePack(_str)));
+        uint rand = uint(keccak256(abi.encodePack(_str)));      // generating random dna using keccak256 with the zombie name
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
-        uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
+        uint randDna = _generateRandomDna(_name);                // get dna
+        _createZombie(_name, randDna);                           // create the zombie
     }
 }
